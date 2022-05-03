95e2d0698447f447b1526828e85862265e12c71c
andrezszambrano <andreskilla2000@gmail.com>
2022-05-03 18:01:42 -0300

    boot_map_region function is implemented and works accordingly with UPAGES region.

--- kern/pmap.c
+++ kern/pmap.c
@@ -183,6 +183,7 @@ mem_init(void)
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
+	boot_map_region(kern_pgdir, UPAGES, PTSIZE, PADDR(pages), PTE_U | PTE_P);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
@@ -195,6 +196,8 @@ mem_init(void)
 	//       overwrite memory.  Known as a "guard page".
 	//     Permissions: kernel RW, user NONE
 	// Your code goes here:
+	//boot_map_region(kern_pgdir, KSTACKTOP - KSTKSIZE, KSTKSIZE, PADDR(bootstack), PTE_W | PTE_P);
+	//boot_map_region(kern_pgdir, KSTACKTOP - PTSIZE, KSTKGAP, 0, PTE_W | PTE_P);
 
 	//////////////////////////////////////////////////////////////////////
 	// Map all of physical memory at KERNBASE.
@@ -204,6 +207,7 @@ mem_init(void)
 	// we just set up the mapping anyway.
 	// Permissions: kernel RW, user NONE
 	// Your code goes here:
+	//boot_map_region(kern_pgdir, KERNBASE, 0xFFFFFFFF - KERNBASE, 0, PTE_W | PTE_P);
 
 	// Check that the initial page directory has been set up correctly.
 	check_kern_pgdir();
@@ -401,6 +405,13 @@ static void
 boot_map_region(pde_t *pgdir, uintptr_t va, size_t size, physaddr_t pa, int perm)
 {
 	// Fill this function in
+	for (uint32_t offset = 0; offset < size; offset+=PGSIZE) {
+		pte_t *pte = pgdir_walk(pgdir, (void*)(va + offset), 1); //create if not created
+//		if (!pte || !pa) 
+//			continue; 
+		*pte = (pa + offset) | perm | PTE_P;
+	}
+	
 }
 
 //
