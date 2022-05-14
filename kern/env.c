d92ab1ce80ff6ac2691d44a2c48b2c58d278d451
andrezszambrano <andreskilla2000@gmail.com>
2022-05-14 17:26:58 -0300

    env_run function is implemented

--- kern/env.c
+++ kern/env.c
@@ -504,6 +504,13 @@ env_run(struct Env *e)
 	//	e->env_tf to sensible values.
 
 	// LAB 3: Your code here.
+	if (curenv && curenv->env_status == ENV_RUNNING)
+			curenv->env_status = ENV_RUNNABLE;
 
-	panic("env_run not yet implemented");
+	curenv = e;
+	curenv->env_status = ENV_RUNNING;
+	curenv->env_runs++;
+	lcr3(PADDR(curenv->env_pgdir));
+	env_pop_tf(&curenv->env_tf);
+	//should not return	
 }
