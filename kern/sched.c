37b32713fc783e391c1f2319bbd1eff8d41e36c8
andrezszambrano <andreskilla2000@gmail.com>
2022-06-07 17:12:27 -0300

    sched_yield implemented

--- kern/sched.c
+++ kern/sched.c
@@ -11,7 +11,7 @@ void sched_halt(void);
 void
 sched_yield(void)
 {
-	struct Env *idle;
+	struct Env *idle = NULL;
 
 	// Implement simple round-robin scheduling.
 	//
@@ -29,6 +29,20 @@ sched_yield(void)
 	// below to halt the cpu.
 
 	// LAB 4: Your code here.
+	size_t starting_index = 0;
+    if (curenv) {
+        starting_index = ENVX(curenv->env_id);
+    }
+    for (size_t j = 0; j < NENV; j++) {
+        size_t index = (starting_index + j) % NENV;
+        if (envs[index].env_status == ENV_RUNNABLE) {
+            env_run(&envs[index]);
+        }
+    }
+    
+    if (curenv && curenv->env_status == ENV_RUNNING) {
+        env_run(curenv);
+    }
 
 	// sched_halt never returns
 	sched_halt();
