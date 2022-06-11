4b7264341777085888863ac0bab2a571a98b1ab1
andrezszambrano <andreskilla2000@gmail.com>
2022-06-11 17:41:31 -0300

    Big kernel_lock is locked and unlocked accordingly

--- kern/init.c
+++ kern/init.c
@@ -50,7 +50,7 @@ i386_init(void)
 
 	// Acquire the big kernel lock before waking up APs
 	// Your code here:
-
+	lock_kernel(&kernel_lock)
 	// Starting non-boot CPUs
 	boot_aps();
 
@@ -131,6 +131,7 @@ mp_main(void)
 	// only one CPU can enter the scheduler at a time!
 	//
 	// Your code here:
+	lock_kernel(&kernel_lock)
 
 	// Remove this after you finish Exercise 4
 	for (;;)
