0e31a2c5d697bb74048d9242f5231891de141c5c
Facundo Mastricchio <facundomastricchio@gmail.com>
2022-07-02 19:54:30 -0300

    Add sys_env_set_trapframe()

--- kern/syscall.c
+++ kern/syscall.c
@@ -138,7 +138,22 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 5: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
-	panic("sys_env_set_trapframe not implemented");
+	user_mem_assert(curenv, tf, sizeof(struct Trapframe), PTE_U);
+
+	struct Env *env;
+	if (envid2env(envid, &env, 1) < 0)
+		return -E_BAD_ENV;
+
+	env->env_tf = *tf;
+	// Modify the trap frame so that user environments run at CPL 3
+	env->env_tf.tf_cs = GD_UT | 3;
+	// interrupts enabled
+	env->env_tf.tf_eflags |= FL_IF;
+	// IOPL to 0
+	env->env_tf.tf_eflags |= FL_IOPL_0;
+	env->env_tf.tf_eflags &= ~FL_IOPL_MASK;
+
+	return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -466,6 +481,9 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		return sys_ipc_try_send((envid_t) a1, a2, (void *) a3, a4);
 	case SYS_env_set_pgfault_upcall:
 		return sys_env_set_pgfault_upcall((envid_t) a1, (void *) a2);
+	case SYS_env_set_trapframe:
+		return sys_env_set_trapframe((envid_t) a1,
+		                             (struct Trapframe *) a2);
 	default:
 		return -E_INVAL;
 	}
