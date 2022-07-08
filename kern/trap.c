a92fafb8fca319d74e29d3a16c9ad522b6b1ef0d
Facundo Mastricchio <facundomastricchio@gmail.com>
2022-07-08 12:21:32 -0300

    Fix keyboard and seriel interrupts
    - Now, commands like 'ls' are working on JOS shell

--- kern/trap.c
+++ kern/trap.c
@@ -263,10 +263,12 @@ trap_dispatch(struct Trapframe *tf)
 	// LAB 5: Your code here.
 	if (tf->tf_trapno == IRQ_KBD + IRQ_OFFSET) {
 		kbd_intr();
+		return;
 	}
 
 	if (tf->tf_trapno == IRQ_SERIAL + IRQ_OFFSET) {
 		serial_intr();
+		return;
 	}
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
